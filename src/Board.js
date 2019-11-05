2d701d845f856b6b1185975a2706e01cae3f8837
peterPalVanyo <vanyo.peter.pal@gmail.com>
2019-11-05 12:20:51 +0100

    board-cell skeleton components

---
+++ src/Board.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import Cell from './Cell'
+
+
+class Board extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return <Cell/>
+    }
+}
+export default Board
---
+++ src/Cell.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class Cell extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return <h1>from the cell</h1>
+    }
+}
+export default Cell
