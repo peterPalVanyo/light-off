e36dd99c640b3f7165cb601c9f0dfaadca75036c
peterPalVanyo <vanyo.peter.pal@gmail.com>
2019-11-06 05:12:59 +0100

    board display and basic csss

--- src/Board.js
+++ src/Board.js
@@ -1,13 +1,51 @@
 import React, {Component} from 'react'
 import Cell from './Cell'
+import './Board.css'
 
 
 class Board extends Component {
+    static defaultProps = {
+        nrows: 5,
+        ncols: 5,
+        chanceToLightOn: 0.4
+    }
+
     constructor(props) {
         super(props)
+        this.state={
+            isOver: false,
+            board:this.createBoard()
+        }
+    }
+    createBoard(){
+        let board = []
+        for(let i=0; i<this.props.nrows; i++) {
+            let row = []
+            for(let i=0; i<this.props.ncols; i++){
+                 row.push(Math.random() < this.props.chanceToLightOn)
+            }
+            board.push(row)
+        }
+
+        return board
     }
     render() {
-        return <Cell/>
+        let tblBoard = []
+        for(let x= 0; x<this.props.nrows; x++) {
+            let row =[] 
+            for (let y = 0; y<this.props.ncols; y++) {
+                let coord=`${y}-${x}`
+                row.push(<Cell key={coord} isOn={this.state.board[y][x]} />)
+            }
+            tblBoard.push(<tr key={x} >{row}</tr>)
+        }
+        return (
+            <table className='Board'>
+                <tbody>
+                        {tblBoard}
+                </tbody>
+            </table>
+        )
     }
 }
 export default Board
--- src/Cell.js
+++ src/Cell.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react'
+import './Cell.css'
 
 class Cell extends Component {
     constructor(props) {
         super(props)
+        this.handleClick = this.handleClick.bind(this)
+    }
+    handleClick(e) {
+
     }
     render() {
-        return <h1>from the cell</h1>
+        const classes = 'Cell' + (this.props.isOn? ' Cell-on': '')
+        return <td className={classes} ></td>
     }
 }
 export default Cell
